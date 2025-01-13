6ee7187e6aab049c2e41d0f58d16898c28523f97
mijail pulgar <mpulgar@abatechnology.com>
2025-01-13 16:38:30 -0500

    feat: registrar usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -8,5 +10,28 @@ export class AuthService extends PrismaClient implements OnModuleInit {
   onModuleInit() {
     this.$connect();
     this.logger.log('MongoDB connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+    try {
+      const user = await this.user.findUnique({ where: { email } });
+      if (user) {
+        throw new RpcException({ status: 400, message: 'User already exists' });
+      }
+      const newUser = await this.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      return {
+        user: newUser,
+        token: 'token',
+      };
+    } catch (error) {
+      throw new RpcException({ status: 400, message: error.message });
+    }
   }
 }
